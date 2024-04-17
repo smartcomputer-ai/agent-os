90808abd2eae0b35e782de87f82e39860ff6633f
lukebuehler <542828+lukebuehler@users.noreply.github.com>
2024-04-17 17:33:14 +0200

    refactor to vars in agent store

--- aos/runtime/apex/apex_core_loop.py
+++ aos/runtime/apex/apex_core_loop.py
@@ -1,4 +1,5 @@
 
+from __future__ import annotations
 from abc import ABC
 from dataclasses import dataclass
 import os
@@ -7,7 +8,7 @@ import grpc
 from aos.grit import *
 from enum import Enum
 from aos.runtime.store import grit_store_pb2, grit_store_pb2_grpc
-from aos.runtime.store import secret_store_pb2_grpc, grit_store_pb2_grpc
+from aos.runtime.store import agent_store_pb2, agent_store_pb2_grpc
 from aos.runtime.apex import apex_api_pb2, apex_workers_pb2
 
 import logging
@@ -48,6 +49,9 @@ class _EventWithCompletion(ABC):
     def set_completion(self):
         self._completion_event.set()
 
+AgentId = ActorId #the agent is defined by the id of the root actor, so technically, it's an actor id too
+AgendDID = str
+WorkerId = str
 
 class ApexCoreLoop:
 
@@ -93,14 +97,14 @@ class ApexCoreLoop:
             return await super().wait_for_result(timeout_seconds)
     
     class _StartAgentEvent(_EventWithCompletion):
-        agent_id:ActorId
-        def __init__(self, agent_id:ActorId) -> None:
+        agent_id:AgentId
+        def __init__(self, agent_id:AgentId) -> None:
             super().__init__()
             self.agent_id = agent_id
 
     class _StopAgentEvent(_EventWithCompletion):
-        agent_id:ActorId
-        def __init__(self, agent_id:ActorId) -> None:
+        agent_id:AgentId
+        def __init__(self, agent_id:AgentId) -> None:
             super().__init__()
             self.agent_id = agent_id
 
@@ -113,12 +117,29 @@ class ApexCoreLoop:
         async def wait_for_result(self, timeout_seconds:float=15)-> apex_api_pb2.InjectMessageResponse:
             return await super().wait_for_result(timeout_seconds)
     
+    @dataclass(slots=True)
+    class _WorkerState:
+        ticket:str
+        manifest:apex_workers_pb2.WorkerManifest
+        to_worker_queue:asyncio.Queue[apex_workers_pb2.ApexToWorkerMessage]|None = None
+
+        @property
+        def is_connected(self):
+            return self.to_worker_queue is not None
+
+    @dataclass(slots=True)
+    class _ActorInfo:
+        manifest:str
 
     class _CoreLoopState:
-        agents:dict[ActorId, apex_workers_pb2.Agent] = {}
-        actors:dict[ActorId, apex_workers_pb2.Actor] = {}
+        running_agents:dict[AgentId, AgendDID] = {}
+        unassigned_actors:set[ActorId] = {} #are not assigned to a worker
+        assigned_actors:dict[ActorId, WorkerId] = {} #are assigned to a worker
 
-        messages:dict[ActorId, list[apex_api_pb2.Message]] = {}
+        actors:dict[ActorId, ApexCoreLoop._ActorInfo] = {}
+        workers:dict[WorkerId, ApexCoreLoop._WorkerState] = {}
+
+        #messages:dict[ActorId, list[]] = {}
 
 
     #external signaling
@@ -153,15 +174,55 @@ class ApexCoreLoop:
         loop_state = self._CoreLoopState()
         
         async with self._connect_to_store_loop() as channel:
-            store_stub = grit_store_pb2_grpc.GritStoreStub(channel)
-            secret_stub = secret_store_pb2_grpc.SecretStoreStub(channel)   
+            grit_store_stub = grit_store_pb2_grpc.GritStoreStub(channel)
+            agent_store_stub = agent_store_pb2_grpc.AgentStoreStub(channel)   
 
             # get all agents
-            agents_response:grit_store_pb2.GetAgentsResponse = await store_stub.GetAgents(grit_store_pb2.GetAgentsRequest()) 
-            loop_state.agents = {agent_id:apex_workers_pb2.Agent(agent_id=agent_id, agent_did=did, grit_address=self._store_address) for did,agent_id in agents_response.agents.items()}
+            agents_response:agent_store_pb2.GetAgentsResponse = await agent_store_stub.GetAgents(agent_store_pb2.GetAgentsRequest(var_filters={"apex.status","started"})) 
+            loop_state.running_agents = {agent_id:did for did, agent_id in agents_response.agents.items()}
         
             # gather unprocessed messages
             # TODO
+
+            #start processing of main loop
+            await asyncio.sleep(0) #yield to allow other tasks to run
+            self._running_event.set() #signal that apex is about to start
+            while not self._cancel_event.is_set():
+
+                event = None
+                try:
+                    event = await asyncio.wait_for(self._event_queue.get(), 0.05)
+                except asyncio.TimeoutError:
+                    continue #test for cancel (in the while condition) and try again
+                
+                if isinstance(event, self._RegisterWorkerEvent):
+                    #if there is an existing worker with the same id, disconnect it
+                    if event.worker_id in loop_state.workers:
+                        worker_state = loop_state.workers[event.worker_id]
+                        if worker_state.is_connected:
+                            logger.warn(f"RegisterWorkerEvent: Worker {event.worker_id} is already connected, disconnecting it.")
+                            worker_state.to_worker_queue.put_nowait(None)
+                    #add worker with new ticket
+                    loop_state.workers[event.worker_id] = self._WorkerState(
+                        ticket=event.ticket,
+                        manifest=event.manifest)
+                    logger.info(f"RegisterWorkerEvent: Worker {event.worker_id} registered.")
+
+                elif isinstance(event, self._WorkerConnectedEvent):
+                    if event.worker_id not in loop_state.workers:
+                        logger.warn(f"WorkerConnectedEvent: Worker {event.worker_id} trying to connect, but it is not registered, NO-OP.")
+                        event.to_worker_queue.put_nowait(None)
+                    else:
+                        loop_state.workers[event.worker_id].to_worker_queue = event.to_worker_queue
+                        logger.info(f"WorkerConnectedEvent: Worker {event.worker_id} connected.")
+                        #TODO: assign actors to worker, consider existing manifest
+                        # run algo that figures out which workers get which actors, 
+                        # rebalance, send relevant GIVE and YANK messages
+                        # in the first version, implement a greedy algo that assigns all actors to workers
+
+                elif isinstance(event, self._WorkerDisconnectedEvent):
+                    logger.info(f"WorkerDisconnectedEvent: Worker {event.worker_id} disconnected.")
+                    #move workers from state
 
     async def stop(self):
         pass
