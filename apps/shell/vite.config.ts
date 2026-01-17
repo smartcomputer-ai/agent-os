6b58f3b7ec63c8e8f3e51c65326e196c085412c4
lb <542828+lukebuehler@users.noreply.github.com>
2026-01-17 19:31:18 +0100

    added tailwind

--- apps/shell/vite.config.ts
+++ apps/shell/vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   server: {
     proxy: {
       '/api': 'http://localhost:7777',
