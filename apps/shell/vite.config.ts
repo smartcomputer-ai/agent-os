afdd85e12a0785e70dac5d6d7dcd2892b647f6ae
lb <542828+lukebuehler@users.noreply.github.com>
2026-01-17 20:06:14 +0100

    shadcn

--- apps/shell/vite.config.ts
+++ apps/shell/vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from "path"
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   server: {
     proxy: {
       '/api': 'http://localhost:7777',
